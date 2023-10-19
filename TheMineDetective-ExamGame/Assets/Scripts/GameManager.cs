0b55f33bfcdcfe27f688609862b1d9a4bd5589d7
OmphileMatsobe <omphilejmatsobe@gmail.com>
2023-10-19 06:33:34 +0200

    Point & Click actions update

--- TheMineDetective-ExamGame/Assets/Scripts/GameManager.cs
+++ TheMineDetective-ExamGame/Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    Button moveTo, pickUp, takePic, note, putInBag;
+
+    [SerializeField]
+    Image setImage;
+
+    [SerializeField]
+    GameObject display;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveTo.interactable = pickUp.interactable = takePic.interactable = false;
+        note.interactable = putInBag.interactable = false;
+    }
+
+    public void onClickButton(Image imgSet)
+    {
+        Debug.Log("Activating");
+        setImage = imgSet;
+        display.SetActive(true);
+        moveTo.interactable = true;
+    }
+
+    public void onClickClose()
+    {
+        display.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        display.SetActive(false);
         
     }
 }
--- TheMineDetective-ExamGame/Assets/Scripts/PlayerMovement.cs
+++ TheMineDetective-ExamGame/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void activateMove(GameObject obj)
+    {
+       move = Vector2.MoveTowards(transform.position, obj.transform.position, 1);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = move;
     }
 }
